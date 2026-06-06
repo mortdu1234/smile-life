e6b15b13e7b1167a06aacd249a5874e10d980761
mortdu1234 <dr05@orange.fr>
2026-06-06 14:06:10 +0200

    reset

--- app/cards/concrete/personal/marriage.py
+++
@@ -1,4 +0,0 @@
-"""Re-export de MarriageCard depuis flirt.py pour compatibilité des imports."""
-from app.cards.concrete.personal.flirt import MarriageCard, AdulteryCard, FlirtCard, FlirtWithChildCard
-
-__all__ = ["MarriageCard", "AdulteryCard", "FlirtCard", "FlirtWithChildCard"]
